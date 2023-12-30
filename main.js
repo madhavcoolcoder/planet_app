cf5f9af3a1a21e7399509422dd86a8e75d512ca4
madhavcoolcoder <140633581+madhavcoolcoder@users.noreply.github.com>
2023-12-30 22:37:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function start()
+{
+    window.location = "planet_screen.html"
+}
---
+++ planet_screen.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function mercury()
+{
+    window.location = "mercury.html"
+}
+
+function venus()
+{
+    window.location = "venus.html"
+}
+
+function earth()
+{
+    window.location = "earth.html"
+}
+
+function mars()
+{
+    window.location = "mars.html"
+}
+
+function jupiter()
+{
+    window.location = "jupiter.html"
+}
+
+function saturn()
+{
+    window.location = "saturn.html"
+}
+
+function urans()
+{
+    window.location = "urans.html"
+}
+
+function neptune()
+{
+    window.location = "neptune"
+}
